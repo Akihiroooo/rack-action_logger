a19ce55b7ce046edd21c1ed27f12ae4093d8974e
Akihiroooo <akihiro.ota@litalico.co.jp>
2020-02-04 15:46:24 +0900

    :speacker: restore_json_log

--- lib/rack/action_logger/emit_adapter/logger_adapter.rb
+++ lib/rack/action_logger/emit_adapter/logger_adapter.rb
@@ -9,7 +9,7 @@ module Rack::ActionLogger::EmitAdapter
         Rack::ActionLogger.logger.info(hash)
         Rack::ActionLogger.logger.info("ここからエラーで落ちてくれるはず！")
         Rack::ActionLogger.logger.info(hash.to_s)
-        # Rack::ActionLogger.logger.info(JSON.pretty_generate(hash))
+        Rack::ActionLogger.logger.info(JSON.pretty_generate(hash))
       else
         Rack::ActionLogger.logger.info(hash)
       end
