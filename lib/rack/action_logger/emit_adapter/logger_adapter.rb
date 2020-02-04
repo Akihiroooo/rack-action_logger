558c69e9ed667765f456c6f13ad6d4a690ca9bea
Akihiroooo <akihiro.ota@litalico.co.jp>
2020-02-04 14:52:44 +0900

    :art: add_log

--- lib/rack/action_logger/emit_adapter/logger_adapter.rb
+++ lib/rack/action_logger/emit_adapter/logger_adapter.rb
@@ -6,7 +6,10 @@ module Rack::ActionLogger::EmitAdapter
     def self.emit(hash)
       hash = wrap(hash)
       if Rack::ActionLogger.configuration.pretty_print
-        Rack::ActionLogger.logger.info(JSON.pretty_generate(hash))
+        Rack::ActionLogger.logger.info(hash)
+        Rack::ActionLogger.logger.info("ここからエラーで落ちてくれるはず！")
+        Rack::ActionLogger.logger.info(hash.to_s)
+        # Rack::ActionLogger.logger.info(JSON.pretty_generate(hash))
       else
         Rack::ActionLogger.logger.info(hash)
       end
